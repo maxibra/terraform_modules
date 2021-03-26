fcce3336730a039002bccbef57d30f35631c1c15
maxibra <max.braver@gmail.com>
2021-03-27 02:08:09 +0300

    v0.0.3 release

--- vpc/aws/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = concat(aws_vpc.this.*.id, [""])[0]
-}
-
-output "vpc_arn" {
-  description = "The ARN of the VPC"
-  value       = concat(aws_vpc.this.*.arn, [""])[0]
-}
